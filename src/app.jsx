bb1c871236adc4c2c1fee9a7cb5f31e6d573b575
John C. Scott <jcscott@scottcomm.com>
2018-03-27 19:46:44 -0700

    Playing around with React.

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const App = () => {
+import PropTypes from 'prop-types';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>This is a dynamic page.</h1>
+//     </div>
+//   );
+// };
+// export default App;
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = () => {
+    this.props.onClickFunction(this.props.incrementValue);
+  };
+}
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        +{this.props.incrementValue}
+      </button>
+    );
+  }
+}
+
+Button.propTypes = {
+  onClickFunction: PropTypes.func,
+  incrementValue: PropTypes.number
+};
+
+const Result = (props) => {
   return (
-    <div>
-      <h1>This is a React page.</h1>
-    </div>
+    <div>{props.counter}</div>
   );
 };
-export default App;
+
+Result.propTypes = {
+  counter: PropTypes.number
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.incrementCounter = (incrementValue) => {
+      this.setState((prevState) => ({
+        counter: prevState.counter + incrementValue
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  onClickFunction: PropTypes.func,
+  incrementValue: PropTypes.number
+};
+
 ReactDOM.render(<App />, document.getElementById("app"));
